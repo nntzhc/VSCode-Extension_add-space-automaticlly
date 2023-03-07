b18fd53f9e477e77bb5688975ca66660ec66bdaf
nntzhc <1553090730@qq.com>
2023-03-08 03:06:51 +0800

    0.3

--- extension.js
+++ extension.js
@@ -2,58 +2,62 @@
  * @Author: nntzhc 1553090730@qq.com
  * @Date: 2023-03-07 23:36:04
  * @LastEditors: nntzhc 1553090730@qq.com
- * @LastEditTime: 2023-03-08 01:47:35
+ * @LastEditTime: 2023-03-08 03:05:32
  * @FilePath: \add-space-automaticlly\extension.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-function onDidChangeTextDocument(event) {
-  let document = event.document;
-  let editor = vscode.window.activeTextEditor;
-
-  if (editor && editor.document === document) {
-    let lastLine = document.lineAt(document.lineCount - 1);
-    let lastChar = lastLine.text.charAt(lastLine.text.length - 1);
-
-    if (lastChar === ';') {
-      let selection = editor.selection;
-      let text = document.getText(selection);
-      let newText = text.replace(/([+\-*\/=])(?=\S)/g, ' $1 ');
-
-      editor.edit(editBuilder => {
-        editBuilder.replace(selection, newText);
-      });
-    }
-  }
-}
-
+/**
+ * @param {vscode.ExtensionContext} context
+ */
 function activate(context) {
-  console.log('The extension has been activated!');
-
-  let disposable2 = vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
-  context.subscriptions.push(disposable2);
-  vscode.workspace.onDidChangeTextDocument(event => {
-    const editor = vscode.window.activeTextEditor;
-    const document = editor.document;
-    const text = document.getText();
-    const lastCharacter = text.charAt(text.length - 2);
-    const currentCharacter = text.charAt(text.length - 1);
-    const range = new vscode.Range(editor.selection.start, editor.selection.end);
-    if (lastCharacter === ";" && /[\+\-\=\*\/]/.test(currentCharacter)) {
-        editor.edit(editBuilder => {
-            editBuilder.replace(range, ` ${currentCharacter} `);
+  const disposable = vscode.commands.registerCommand(
+    "addSpaceAroundOperator",
+    function () {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        const updatedText = text.replace(/([+\-*/%=])/g, " $1 ");
+        const fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(text.length)
+        );
+        editor.edit((editBuilder) => {
+          editBuilder.replace(fullRange, updatedText);
         });
+      }
     }
-});
+  );
 
+  context.subscriptions.push(disposable);
+
+  // Create a semicolon completion item
+  const semicolonCompletionItem = new vscode.CompletionItem(";");
+  semicolonCompletionItem.command = {
+    command: "addSpaceAroundOperator",
+    title: "Add Space Around Operator",
+  };
+
+  // Register the semicolon completion item provider
+  const documentSelector = { scheme: "file", language: "plaintext" };
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      documentSelector,
+      {
+        provideCompletionItems() {
+          return [semicolonCompletionItem];
+        },
+      },
+      ";"
+    )
+  );
 }
 
-function deactivate() {
-  console.log('The extension has been deactivated!');
-}
+function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
