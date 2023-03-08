87e26028e3a49e6cabea90e36ca8e794b50271a5
nntzhc <1553090730@qq.com>
2023-03-08 11:04:44 +0800

    0.1

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
  * @Author: nntzhc 1553090730@qq.com
  * @Date: 2023-03-07 23:36:04
  * @LastEditors: nntzhc 1553090730@qq.com
- * @LastEditTime: 2023-03-08 03:05:32
+ * @LastEditTime: 2023-03-08 09:46:15
  * @FilePath: \add-space-automaticlly\extension.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,17 +17,31 @@ function activate(context) {
     function () {
       const editor = vscode.window.activeTextEditor;
       if (editor) {
-        const document = editor.document;
-        const text = document.getText();
-        const updatedText = text.replace(/([+\-*/%=])/g, " $1 ");
-        const fullRange = new vscode.Range(
-          document.positionAt(0),
-          document.positionAt(text.length)
-        );
-        editor.edit((editBuilder) => {
-          editBuilder.replace(fullRange, updatedText);
-        });
+      const currentLine = editor.document.lineAt(editor.selection.active.line).text;
+      const operators = ['+', '-', '*', '/', '=', '>', '<', '%', '&', '|', '^', '!'];
+      let newLine = '';
+      for (let i = 0; i < currentLine.length-1; i++) {
+        const char = currentLine[i];
+        const prevChar = currentLine[i - 1];
+        const nextChar = currentLine[i + 1];
+        if (operators.includes(char)) {
+          if (prevChar !== ' ') {
+            newLine += ' ';
+          }
+          newLine += char;
+          if (nextChar !== ' ' && nextChar !== undefined) {
+            newLine += ' ';
+          }
+        } else {
+          newLine += char;
+        }
       }
+      editor.edit(editBuilder => {
+        const start = new vscode.Position(editor.selection.active.line, 0);
+        const end = new vscode.Position(editor.selection.active.line, currentLine.length);
+        editBuilder.replace(new vscode.Range(start, end), newLine);
+      });
+    }
     }
   );
 
